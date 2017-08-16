0df7f7e4efd1703588caee6782463271878d2eb3
meafmira <gang7e@gmail.com>
2017-08-16 12:41:23 +0300

    Push-all script

---
+++ bin/push-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+langs[1]=javascript
+langs[2]=java
+langs[3]=csharp
+langs[4]=ruby
+langs[5]=php
+
+for lang in $(seq 1 5)
+  do
+    cd sdk/${langs[lang]}
+    git add .
+    git commit -m $1
+    git push
+    cd ../..
+  done
