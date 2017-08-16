7184b1823a59a006527d758494bab7d048b772bc
meafmira <gang7e@gmail.com>
2017-08-16 12:12:18 +0300

    Added shell scripts

---
+++ bin/generate-sdk.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+output="sdk"
+
+if [ $# -eq 0 ]
+then
+  echo "No arguments supplied. Creating all sdks..."
+
+  langs[1]=javascript
+  langs[2]=java
+  langs[3]=csharp
+  langs[4]=ruby
+  langs[5]=php
+
+  for lang in $(seq 1 5)
+  do
+    $BASH_SOURCE -l ${langs[lang]} -o sdk
+  done
+else
+  OPTIND=1
+
+  output=sdk
+  lang=javascript
+
+  while getopts ":o:l:" opt; do
+    case $opt in
+      o)
+        output=$OPTARG
+        echo "Output directory: $OPTARG" >&2
+        ;;
+      l)
+        lang=$OPTARG
+        echo "Lang: $OPTARG" >&2
+        ;;
+      \?)
+        echo "Invalid option: -$OPTARG" >&2
+        ;;
+      :)
+        echo "Option -$OPTARG requires an argument." >&2
+        exit 1
+        ;;
+    esac
+  done
+
+  shift $((OPTIND-1))
+
+  [ "$1" = "--" ] && shift
+  java -jar swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i schema.json -l $lang -o $output/$lang
+fi
